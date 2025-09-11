b802bfceead1c9dbfad6444b423191d9b813746c
HasiburRahman-hr11 <mdhasiburrahman47@gmail.com>
2025-09-11 18:39:42 +0600

    slider condition

--- Components/Homepage/Testimonials/Testimonials.jsx
+++ Components/Homepage/Testimonials/Testimonials.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import Slider from "react-slick";
 import Image from "next/image";
 import Link from "next/link";
@@ -69,6 +69,7 @@ const Testimonials = () => {
   const sliderRef = useRef(null);
   const videoRefs = useRef([]);
   const progressRefs = useRef([]);
+  const [matches, setMatches] = useState(false);
 
   let sliderSettings = {
     dots: false,
@@ -159,6 +160,22 @@ const Testimonials = () => {
     }
   };
 
+  function useMediaQuery(query) {
+    useEffect(() => {
+      const media = window.matchMedia(query);
+      if (media.matches !== matches) {
+        setMatches(media.matches);
+      }
+      const listener = () => setMatches(media.matches);
+      media.addEventListener("change", listener);
+      return () => media.removeEventListener("change", listener);
+    }, [matches, query]);
+
+    return matches;
+  }
+
+  const isMobile = useMediaQuery("(max-width: 1023px)");
+
   return (
     <section className="bg-[#0D0D0D] py-[6rem] overflow-hidden xl:py-[13rem]">
       <div className="container">
@@ -172,138 +189,141 @@ const Testimonials = () => {
           </h2>
         </div>
       </div>
+      {isMobile ? (
+        <div className=" relative mx-auto mt-[4rem] hp-testimonial-slider pl-[2rem] lg:pl-[30px] xl:w-[90%] xl:px-[20px] xl:mt-[7rem]">
+          <Slider ref={sliderRef} {...sliderSettings}>
+            {testimonials.map((testimonial, index) => (
+              <div key={index} className="h-full">
+                <div
+                  className="relative h-full w-full overflow-hidden rounded-[1.5rem] max-h-[75rem] slide-wrapper xl:rounded-[2rem]"
+                  onClick={() => {
+                    const video = videoRefs.current[index];
+                    if (video && !video.paused) handlePause(index);
+                  }}
+                >
+                  <div className="video-box h-full w-[85vw] md:w-[43vw] lg:w-[32vw] xl:w-[27vw]">
+                    <video
+                      ref={(el) => (videoRefs.current[index] = el)}
+                      src={testimonial.video}
+                      poster={testimonial.thumbnail}
+                      className="w-full h-full object-cover block"
+                      preload="metadata"
+                      playsInline
+                      controls={false}
+                      loop={false}
+                      onPause={() => handlePause(index)}
+                      onEnded={() => handleEnded(index)}
+                      onTimeUpdate={() => handleTimeUpdate(index)}
+                    ></video>
+                  </div>
+
+                  <div className="content absolute left-0 top-0 w-full h-full flex flex-col justify-end px-[1.5rem] py-[3rem] xl:px-[3rem] xl:py-[4rem]">
+                    <div className="flex justify-between items-end">
+                      <div className="flex-[1] pr-[2rem]">
+                        <h6 className="text-[2rem] text-white font-medium xl:text-[2.5rem]">
+                          {testimonial.name}
+                        </h6>
+                        <span className="text-[1.2rem] text-[#C8C8C8] xl:text-[1.6rem]">
+                          {testimonial.designation}
+                        </span>
+                      </div>
+                      <span
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handlePlayClick(index);
+                        }}
+                        className="cursor-pointer w-[3.5rem] h-[3.5rem] rounded-[50%] overflow-hidden inline-flex play-icon bg-black/60 transition-all duration-300 xl:w-[5rem] xl:h-[5rem]"
+                      >
+                        <img
+                          src="/images/play-icon-red.webp"
+                          alt=""
+                          className="w-full h-full object-contain"
+                        />
+                      </span>
+                    </div>
+
+                    <div className="progress-bar w-full h-[0.3rem] rounded-[1rem] overflow-hidden bg-[#5D5D5D] mt-[1.5rem] relative xl:mt-[2.5rem] xl:h-[0.47rem]">
+                      <div
+                        ref={(el) => (progressRefs.current[index] = el)}
+                        className="fill w-0 bg-white h-full absolute left-0 top-0 transition-[width] duration-200"
+                      ></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      ) : (
+        <div className="relative mx-auto mt-[4rem] hp-testimonial-slider pl-[2rem] lg:pl-[30px] xl:w-[90%] xl:px-[25px] xl:mt-[7rem]">
+          <Slider ref={sliderRef} {...sliderSettingsDesk}>
+            {testimonials.map((testimonial, index) => (
+              <div key={index} className="h-full">
+                <div
+                  className="relative h-full w-full overflow-hidden rounded-[1.5rem] max-h-[75rem] slide-wrapper xl:rounded-[2rem]"
+                  onClick={() => {
+                    const video = videoRefs.current[index];
+                    if (video && !video.paused) handlePause(index);
+                  }}
+                >
+                  <div className="video-box h-full w-[85vw] md:w-[43vw] lg:w-full lg:h-[75rem]">
+                    <video
+                      ref={(el) => (videoRefs.current[index] = el)}
+                      src={testimonial.video}
+                      poster={testimonial.thumbnail}
+                      className="w-full h-full object-cover block"
+                      preload="metadata"
+                      playsInline
+                      controls={false}
+                      loop={false}
+                      onPause={() => handlePause(index)}
+                      onEnded={() => handleEnded(index)}
+                      onTimeUpdate={() => handleTimeUpdate(index)}
+                    ></video>
+                  </div>
+
+                  <div className="content absolute left-0 top-0 w-full h-full flex flex-col justify-end px-[1.5rem] py-[3rem] xl:px-[3rem] xl:py-[4rem]">
+                    <div className="flex justify-between items-end">
+                      <div className="flex-[1] pr-[2rem]">
+                        <h6 className="text-[2rem] text-white font-medium xl:text-[2.5rem]">
+                          {testimonial.name}
+                        </h6>
+                        <span className="text-[1.2rem] text-[#C8C8C8] xl:text-[1.6rem]">
+                          {testimonial.designation}
+                        </span>
+                      </div>
+                      <span
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handlePlayClick(index);
+                        }}
+                        className="cursor-pointer w-[3.5rem] h-[3.5rem] rounded-[50%] overflow-hidden inline-flex play-icon bg-black/60 transition-all duration-300 xl:w-[5rem] xl:h-[5rem]"
+                      >
+                        <img
+                          src="/images/play-icon-red.webp"
+                          alt=""
+                          className="w-full h-full object-contain"
+                        />
+                      </span>
+                    </div>
+
+                    <div className="progress-bar w-full h-[0.3rem] rounded-[1rem] overflow-hidden bg-[#5D5D5D] mt-[1.5rem] relative xl:mt-[2.5rem] xl:h-[0.47rem]">
+                      <div
+                        ref={(el) => (progressRefs.current[index] = el)}
+                        className="fill w-0 bg-white h-full absolute left-0 top-0 transition-[width] duration-200"
+                      ></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      )}
 
       {/* Video SLider Mobile */}
-      <div className=" relative mx-auto mt-[4rem] hp-testimonial-slider pl-[2rem] lg:pl-[30px] xl:w-[90%] xl:px-[20px] xl:mt-[7rem] lg:hidden">
-        <Slider ref={sliderRef} {...sliderSettings}>
-          {testimonials.map((testimonial, index) => (
-            <div key={index} className="h-full">
-              <div
-                className="relative h-full w-full overflow-hidden rounded-[1.5rem] max-h-[75rem] slide-wrapper xl:rounded-[2rem]"
-                onClick={() => {
-                  const video = videoRefs.current[index];
-                  if (video && !video.paused) handlePause(index);
-                }}
-              >
-                <div className="video-box h-full w-[85vw] md:w-[43vw] lg:w-[32vw] xl:w-[27vw]">
-                  <video
-                    ref={(el) => (videoRefs.current[index] = el)}
-                    src={testimonial.video}
-                    poster={testimonial.thumbnail}
-                    className="w-full h-full object-cover block"
-                    preload="metadata"
-                    playsInline
-                    controls={false}
-                    loop={false}
-                    onPause={() => handlePause(index)}
-                    onEnded={() => handleEnded(index)}
-                    onTimeUpdate={() => handleTimeUpdate(index)}
-                  ></video>
-                </div>
-
-                <div className="content absolute left-0 top-0 w-full h-full flex flex-col justify-end px-[1.5rem] py-[3rem] xl:px-[3rem] xl:py-[4rem]">
-                  <div className="flex justify-between items-end">
-                    <div className="flex-[1] pr-[2rem]">
-                      <h6 className="text-[2rem] text-white font-medium xl:text-[2.5rem]">
-                        {testimonial.name}
-                      </h6>
-                      <span className="text-[1.2rem] text-[#C8C8C8] xl:text-[1.6rem]">
-                        {testimonial.designation}
-                      </span>
-                    </div>
-                    <span
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        handlePlayClick(index);
-                      }}
-                      className="cursor-pointer w-[3.5rem] h-[3.5rem] rounded-[50%] overflow-hidden inline-flex play-icon bg-black/60 transition-all duration-300 xl:w-[5rem] xl:h-[5rem]"
-                    >
-                      <img
-                        src="/images/play-icon-red.webp"
-                        alt=""
-                        className="w-full h-full object-contain"
-                      />
-                    </span>
-                  </div>
-
-                  <div className="progress-bar w-full h-[0.3rem] rounded-[1rem] overflow-hidden bg-[#5D5D5D] mt-[1.5rem] relative xl:mt-[2.5rem] xl:h-[0.47rem]">
-                    <div
-                      ref={(el) => (progressRefs.current[index] = el)}
-                      className="fill w-0 bg-white h-full absolute left-0 top-0 transition-[width] duration-200"
-                    ></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
 
       {/* Video Slider Desktop */}
-      <div className="hidden lg:block relative mx-auto mt-[4rem] hp-testimonial-slider pl-[2rem] lg:pl-[30px] xl:w-[90%] xl:px-[25px] xl:mt-[7rem]">
-        <Slider ref={sliderRef} {...sliderSettingsDesk}>
-          {testimonials.map((testimonial, index) => (
-            <div key={index} className="h-full">
-              <div
-                className="relative h-full w-full overflow-hidden rounded-[1.5rem] max-h-[75rem] slide-wrapper xl:rounded-[2rem]"
-                onClick={() => {
-                  const video = videoRefs.current[index];
-                  if (video && !video.paused) handlePause(index);
-                }}
-              >
-                <div className="video-box h-full w-[85vw] md:w-[43vw] lg:w-full lg:h-[75rem]">
-                  <video
-                    ref={(el) => (videoRefs.current[index] = el)}
-                    src={testimonial.video}
-                    poster={testimonial.thumbnail}
-                    className="w-full h-full object-cover block"
-                    preload="metadata"
-                    playsInline
-                    controls={false}
-                    loop={false}
-                    onPause={() => handlePause(index)}
-                    onEnded={() => handleEnded(index)}
-                    onTimeUpdate={() => handleTimeUpdate(index)}
-                  ></video>
-                </div>
-
-                <div className="content absolute left-0 top-0 w-full h-full flex flex-col justify-end px-[1.5rem] py-[3rem] xl:px-[3rem] xl:py-[4rem]">
-                  <div className="flex justify-between items-end">
-                    <div className="flex-[1] pr-[2rem]">
-                      <h6 className="text-[2rem] text-white font-medium xl:text-[2.5rem]">
-                        {testimonial.name}
-                      </h6>
-                      <span className="text-[1.2rem] text-[#C8C8C8] xl:text-[1.6rem]">
-                        {testimonial.designation}
-                      </span>
-                    </div>
-                    <span
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        handlePlayClick(index);
-                      }}
-                      className="cursor-pointer w-[3.5rem] h-[3.5rem] rounded-[50%] overflow-hidden inline-flex play-icon bg-black/60 transition-all duration-300 xl:w-[5rem] xl:h-[5rem]"
-                    >
-                      <img
-                        src="/images/play-icon-red.webp"
-                        alt=""
-                        className="w-full h-full object-contain"
-                      />
-                    </span>
-                  </div>
-
-                  <div className="progress-bar w-full h-[0.3rem] rounded-[1rem] overflow-hidden bg-[#5D5D5D] mt-[1.5rem] relative xl:mt-[2.5rem] xl:h-[0.47rem]">
-                    <div
-                      ref={(el) => (progressRefs.current[index] = el)}
-                      className="fill w-0 bg-white h-full absolute left-0 top-0 transition-[width] duration-200"
-                    ></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
 
       <div className="mt-[5rem] lg:hidden w-[85%] mx-auto max-w-[40rem]">
         <Link
